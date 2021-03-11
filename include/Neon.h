e20981104e69f86d0cc70d68f962be9d10623400
one30 <837603674@qq.com>
2021-03-11 09:41:45 +0800

    add .git

---
+++ include/Neon.h
@@ -0,0 +1,24 @@
+/*
+ * @Descripttion : 
+ * @Version      : 
+ * @Autor        : one30: one30@m.scnu.edu.cn(email)
+ * @Date         : 2021-03-10 22:39:40
+ * @LastEditTime : 2021-03-10 22:47:50
+ * @FilePath     : /include/Neon.h
+ */
+/* ******************************************** *\
+ * 
+ *  gcc -O3 -std=gnu11 -mfpu=neon -flax-vector-conversions 
+ *
+\* ******************************************** */
+
+
+#pragma once
+#include <stdlib.h>
+#include <arm_neon.h>
+#include <stdint.h>
+
+#ifndef Neon
+#define Neon
+#endif
+
